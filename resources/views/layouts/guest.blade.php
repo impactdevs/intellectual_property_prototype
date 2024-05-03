8bdc023c1b3a7a881047cd54412ebea088ebe29b
dave <dave>
2024-05-03 17:48:28 +0300

    tittle edit

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -5,8 +5,12 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Intellectual Property rights portal') }}</title>
+        <title>Intellectual Property rights portal</title>
 
+        <!-- Favicons -->
+        <link href="{{ asset('assets/img/logo.jpg') }}" rel="icon">
+        <link href="{{ asset('assets/img/logo.jpg') }}" rel="apple-touch-icon">
+        
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
